93d12deded717cb667edfd0d185d16283c68967c
JYOSHNA MANI ADDALA <45555841+jyoshnamaniaddala@users.noreply.github.com>
2019-02-14 22:48:41 +0530

    updated code

--- catalog/menuitems.py
+++ catalog/menuitems.py
@@ -1,50 +1,36 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from database_setup import User, States, Base, MenuItem
+from database_setup import User,States, Base, MenuItem
 
-engine = create_engine('sqlite:///projectdatabase.db')
+engine = create_engine('postgresql://catalog:catalog@localhost/catalog')
+
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
 session = DBSession()
-user1 = User(name="admin", email="jyoshnamaniaddala@gmail.com")
+user1=User(name="admin",email="jyoshnamaniaddala@gmail.com",)
 session.add(user1)
 session.commit()
 
-state1 = States(name="Andhra pradesh", user_id=1)
+# Menu for UrbanBurger
+state1 = States(name="Andhra pradesh",user_id=1)
 
 session.add(state1)
 session.commit()
 
-menuItem1 = MenuItem(name="pulihora", state=state1, user_id=1)
+menuItem1 = MenuItem(name="pulihora",ingredients="rice,tamrind,oil",image_url="https://www.google.com/search?q=pulihora&client=firefox-b-d&source=lnms&tbm=isch&sa=X&ved=0ahUKEwit47ng-rrgAhWJgI8KHXzMDu0Q_AUIDigB#",description="made of rice",itemtype="rice item",link="http://youtube.com",state=state1,user_id=1)
 
 session.add(menuItem1)
 session.commit()
-menuItem2 = MenuItem(name="pansapattu curry", state=state1, user_id=1)
 
-session.add(menuItem2)
-session.commit()
-menuItem3 = MenuItem(name="palakova", state=state1, user_id=1)
 
-session.add(menuItem3)
-session.commit()
-state2 = States(name="Telangana", user_id=1)
-
-session.add(state2)
-session.commit()
-
-menuItem1 = MenuItem(name="hyderabadi dum birayani", state=state2, user_id=1)
-
-session.add(menuItem1)
-session.commit()
-menuItem2 = MenuItem(name="haleem", state=state2, user_id=1)
-
-session.add(menuItem2)
-session.commit()
-menuItem3 = MenuItem(name="gongura chutney", state=state2, user_id=1)
-
-session.add(menuItem3)
-session.commit()
